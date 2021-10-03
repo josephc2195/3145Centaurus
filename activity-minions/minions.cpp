7df1352b80f56c6cb28ed69155318835a88b0e86
josephc2195 <chicaj1221@gmail.com>
2021-10-02 22:15:37 -0400

    Update minions.cpp

--- activity-minions/minions.cpp
+++ activity-minions/minions.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <thread>
 
-
+void f(int i) {
+    std::cout << "Hello! I am minion " << i << std::endl;
+}
 int main (int argc, char** argv) {
   if (argc < 2) {
     std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
     return -1;
   }
   //Your code goes here
-  
+  for (int i = 0; i < argc[1]; i++) {
+      std::thread gru[i](f, i);
+  }
+
+  for (int i = 0; i < argc[1]; i++) {
+      std::thread gru[i].join();
+  }
+
   return 0;
 }
