dd6540b94066dc18f9686e048b891a844fa5eba4
josephc2195 <chicaj1221@gmail.com>
2021-10-02 23:13:12 -0400

    Update minions.cpp

--- activity-minions/minions.cpp
+++ activity-minions/minions.cpp
@@ -4,6 +4,7 @@
 void f(int i) {
     std::cout << "Hello! I am minion " << i << std::endl;
 }
+
 int main (int argc, char** argv) {
   if (argc < 2) {
     std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
@@ -11,13 +12,18 @@ int main (int argc, char** argv) {
   }
   //Your code goes here
   int num = atoi(argv[2]);
-  for (int i = 0; i < num; i++) {
+  
+  std::thread gru(f, num);
+  gru.join();
+
+  
+  /*for (int i = 0; i < num; i++) {
       std::thread gru[i](f, i);
   }
 
   for (int i = 0; i < num; i++) {
-      std::thread gru[i].join();
-  }
+      gru[i].join();
+  }*/
 
   return 0;
 }
