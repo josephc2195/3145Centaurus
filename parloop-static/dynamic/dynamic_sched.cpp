fc3d7a6a0514d0efe0cd1b53f0872ec51efbb3f0
josephc2195 <chicaj1221@gmail.com>
2021-10-26 14:38:42 -0400

    update

--- parloop-static/dynamic/dynamic_sched.cpp
+++ parloop-static/dynamic/dynamic_sched.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "seq_loop.hpp"
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -23,6 +25,22 @@ int main (int argc, char* argv[]) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <granularity>"<<std::endl;
     return -1;
   }
+
+  int fID = atoi(argv[1]);
+  int a = atoi(argv[2]);
+  int b = atoi(argv[3]);
+  int n = atoi(argv[4]);
+  int intensity = atoi(argv[5]);
+  int nbthreads = atoi(argv[6]);
+  int g = atoi(argv[7]);
+  float t1 = (b-a)/n;
+  float sum = 0;
+
+
+  SeqLoop s1;
+
+  auto start = std::chrono::steady_clock::now();
+  
 
   return 0;
 }
