57ebb1df6dacb02a3701de15dfa0fadb5fdd2a8f
josephc2195 <chicaj1221@gmail.com>
2021-12-05 17:16:12 -0500

    Update hello.cpp

--- mpi-hello-world/hello_world/hello.cpp
+++ mpi-hello-world/hello_world/hello.cpp
@@ -6,15 +6,15 @@ int main(int argc, char*argv[]) {
     
     MPI_Init (&argc, &argv);
     
-    int process_ID, total;
+    int pid, total;
     
     MPI_Comm_size(MPI_COMM_WORLD, &total);
-    MPI_Comm_rank(MPI_COMM_WORLD, &process_ID)
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid)
     
     char host[256];
-    int result = gethostname(host, sizeof(name));
+    int result = gethostname(host, sizeof(host));
     
     int i = getpid();
-    std::cout <<"I am process " <<i<<" out of "<<total<< ". I am running on "<<host<<"\n"
+    std::cout <<"I am process " <<i<<" out of "<<total<< ". I am running on "<<host<<"\n";
     MPI_Finalize();
 }
